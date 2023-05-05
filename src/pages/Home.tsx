326f22c5357878a65cfce966b9106a4ab366791f
Minkaill <mkx.dake@gmail.com>
2023-05-05 15:52:43 +0300

    Добавил навигацию по авторизации

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,21 @@
+import React from "react";
 import { Header } from "../components/Header";
 import { TodoList } from "../components/TodoList";
+import { userData } from "../utils/helper";
+import { useNavigate } from "react-router-dom";
 
 export const Home = () => {
+  const { token } = userData();
+  const navigate = useNavigate();
+
+  React.useEffect(() => {
+    if (!token) {
+      navigate("/login");
+    } else {
+      navigate("/");
+    }
+  }, []);
+
   return (
     <>
       <Header />
